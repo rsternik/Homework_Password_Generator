aab765e4cfece0a3f0d378c9621d096b4103b29d
rsternik <rsternik.wg@gmail.com>
2021-09-30 00:50:31 -0700

    password generation operational. However, script runs on page load and not on button click. and password length currently has no bounds character limit.

--- script.js
+++ script.js
@@ -1,68 +1,51 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+// Character Set
+var upperCase = "ABCDEFG";
+var lowerCase = "abcdefg";
+var number = "0123456789";
+var special = "!@#$%^&*()-+";
+var charBase = "";
+var randomChar = "";
+
+
+
 
 // Write password to the #password input
 function writePassword() {
-  // var password = generatePassword();
+
   var passwordText = document.querySelector("#password");
-  var upperCase = "ABCDEFG"
-  var lowerCase = upperCase.toLowerCase();
-  var number = "0123456789";
-  var special = "!@#$%^&*()-+";
-  var charBase = "";
+  
+
+  //  User Input
   var passwordLength = parseInt(prompt("How long to you want your password to be?"));
   var lowerConf = confirm("Do you want lowercase characters?");
   var upperConf = confirm("Do you want uppercase characters?");
   var specialConf = confirm("Do you want any special characters?");
   var numberConf = confirm("Do you want to include numbers?");
-  var finalPassword = "";
-
-  passwordText.value = password;
-  // function here
-
-  // Inputs for password length, yes or no for special characters
-
-  //   Conditions
-
-  //   - 8 - 128 Characters, or weather not to include lowercase, uppercase, numeric, and / or special characters
-
+  // Conditions
 
   if (lowerConf) {
-    charBase = charBase + lowerCase;
+    charBase += lowerCase;
   }
   if (upperConf) {
-    charBase = charBase + upperCase;
+    charBase += upperCase;
   }
   if (numberConf) {
-    charBase = charBase + number;
+    charBase += number;
   }
   if (specialConf) {
-    charBase = charBase + special;
+    charBase += special;
   }
 
+  // Loop
   for (let i = 0; i < passwordLength; i++) {
 
-    let randomChar;
-    randomChar = charBase.charAt(Math.floor(Math.random() * charBase.length));
-    finalPassword += randomChar;
     
-
+    randomChar += charBase.charAt(Math.floor(Math.random() * charBase.length));
+    passwordText.value = randomChar;
   }
-  return finalPassword;
-
 }
-
-
-
-
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
 
-
-
-
-
-
-
-// return
-
+generateBtn.addEventListener("click", writePassword());
